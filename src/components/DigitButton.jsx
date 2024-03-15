9c3f302ded7c91d995d1280478b7cc4932b3730e
Lauri-Keuda <lauri.riihimaki@edu.keuda.fi>
2024-03-15 13:24:16 +0200

    Updates

--- src/components/DigitButton.jsx
+++
@@ -1,9 +0,0 @@
-const DigitButton = ({ value, add}) => {
-  return (
-    <button className="calc-button" onClick={() => {add(value)}} type="button">
-      {value}
-    </button>
-  )
-}
-
-export default DigitButton
--- src/components/OperationButton.jsx
+++
@@ -1,9 +0,0 @@
-const OperationButton = ({ value, operate}) => {
-  return (
-    <button className={`calc-button${value === "=" ? " span2" : ""}`} onClick={() => {operate(value)}}  type="button">
-      {value}
-    </button>
-  )
-}
-
-export default OperationButton
